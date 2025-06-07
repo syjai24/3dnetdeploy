7721e99e6850f6a9623de543bf83c90677c9bad0
syjai24 <syjai24coder@163.com>
2025-06-07 22:15:47 +0800

    a

---
+++ acode/git.sh
@@ -0,0 +1,15 @@
+关联：
+(openmmlabtest) (base) yijian@yijian:~/netcode/3dnettest$ git remote add origin https://github.com/syjai24/3dnetdeploy.git
+
+提交：
+(openmmlabtest) (base) yijian@yijian:~/netcode/3dnettest$ git commit -m "first"
+
+上传：
+(openmmlabtest) (base) yijian@yijian:~/netcode/3dnettest$ git push -u origin main
+
+
+写出账号名与邮箱：
+(openmmlabtest) (base) yijian@yijian:~/netcode/3dnettest$ git config --global user.email "syjai24coder@163.com"
+(openmmlabtest) (base) yijian@yijian:~/netcode/3dnettest$ 
+git config --global user.name "syjai24"
+
